6c165770822c6c421d056bc7148c74bc525c10c2
SameerUH <sameerulhaq2005@gmail.com>
2025-02-12 23:29:14 +0000

    Summary: Made progress on variables and figuring out how I can show it on GitHub. See Journal(12.02.2025)

---
+++ Day 3/Learning about variables.js
@@ -0,0 +1,43 @@
+"use strict";
+/*
+var height;
+console.log(height); // -> undefined
+console.log(weight); // -> Uncaught ReferenceError: weight is not defined
+
+let height;
+console.log(height); // -> undefined.
+
+var height;
+var height;
+console.log(height); // -> undefined.
+
+//let height;
+//let height;
+//console.log(height);
+
+
+let height = 180;
+let anotherHeight = height;
+let weight;
+console.log(height); // -> 180
+console.log(anotherHeight); // -> 180
+weight = 70;
+console.log(weight); // -> 70
+
+height = 180;
+console.log(height);
+
+let  steps  =  100;
+console.log(steps);  //  ->  100
+steps  =  120;  //  ->  120
+console.log(steps);
+steps  =  steps  +  200;
+console.log(steps);  //  ->  320
+
+let greeting = "Hello!";
+let counter = 100;
+
+console.log(greeting); // -> Hello!
+greeting = 1;
+console.log(greeting); // -> 1
+*/
